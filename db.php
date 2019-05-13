95ef588e6b9982d86f8ec52134db223d5e192217
sammykhoooo <samanthakimkaijing@gmail.com>
2019-05-13 14:03:54 +0800

    first commit

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php // define database connection variables as constant
+
+define('DB_USER', "root"); // db user
+
+define('DB_PASSWORD', ""); // db password
+
+define('DB_DATABASE', "16_Samantha(2)_pessdb"); // database name
+
+define('DB_SERVER', "localhost"); // db server
+
+?>
